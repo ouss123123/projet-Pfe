c1bd5b82fae48e5073abc4aded3b8199be609ffa
OuachouX7 <ouachou9@gmail.com>
2025-05-01 02:13:52 +0200

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { lazy } from "react";
 import { Suspense } from "react";
 import Spinner from "./Components/Loading/Spinner";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+const Home = lazy(() => import("./Pages/Home/Home"));
 const SignUp = lazy(() => import("./Pages/Auth/SignUp"));
 const Login = lazy(() => import("./Pages/Auth/Login"));
 const NotFound = lazy(() => import("./Pages/NotFound/NotFound"));
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Suspense fallback={<Spinner />}>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
