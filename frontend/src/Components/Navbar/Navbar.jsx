9f58e7bd1258bc44302197292925a53aa587d877
OuachouX7 <ouachou9@gmail.com>
2025-05-03 22:21:51 +0200

    added pagination

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFutbol } from '@fortawesome/free-solid-svg-icons';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFutbol } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <FontAwesomeIcon
-            icon={faFutbol}
-            size="lg"
-            style={{ color: "#4caf50" }}
-          />{" "}
-          <Link to="/">TeamUpPlay</Link>
-        </div>
-        <ul className="navbar-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-        <ul className="navbar-auth-links">
-          <li className="login-btn">
-            <Link to="/login">Login</Link>
-          </li>
-          <li className="signup-btn">
-            <Link to="/signup">Signup</Link>
-          </li>
-        </ul>
-        
-      </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <FontAwesomeIcon
+          icon={faFutbol}
+          size="lg"
+          style={{ color: "#4caf50" }}
+        />{" "}
+        <Link to="/">TeamUpPlay</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <ul className="navbar-auth-links">
+        <li className="login-btn">
+          <Link to="/login">Login</Link>
+        </li>
+        <li className="signup-btn">
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
