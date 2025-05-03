c0e1883457573147cc615af6d9178369b7dce054
OuachouX7 <ouachou9@gmail.com>
2025-05-03 22:43:38 +0200

    added translation

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFutbol } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const { t, i18n } = useTranslation();
+  const handleLanguage = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
+
+  useEffect(() => {
+    const lang = i18n.language;
+    lang === "ar" ? (document.body.dir = "rtl") : "ltr";
+  }, [i18n.language]);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -13,30 +24,35 @@ const Navbar = () => {
           size="lg"
           style={{ color: "#4caf50" }}
         />{" "}
-        <Link to="/">TeamUpPlay</Link>
+        <Link to="/">{t("TeamUpPlay")}</Link>
       </div>
       <ul className="navbar-links">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">{t("Home")}</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">{t("About")}</Link>
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link to="/services">{t("Services")}</Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact">{t("Contact")}</Link>
         </li>
       </ul>
       <ul className="navbar-auth-links">
         <li className="login-btn">
-          <Link to="/login">Login</Link>
+          <Link to="/login">{t("Login")}</Link>
         </li>
         <li className="signup-btn">
-          <Link to="/signup">Signup</Link>
+          <Link to="/signup">{t("Signup")}</Link>
         </li>
       </ul>
+      <select onChange={handleLanguage}>
+        <option value="fr">fr</option>
+        <option value="ar">ar</option>
+        <option value="en">en</option>
+      </select>
     </nav>
   );
 };
