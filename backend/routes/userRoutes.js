3d2382bc4bb65702a1fe4b508f20db1e9a287168
OuachouX7 <ouachou9@gmail.com>
2025-05-06 20:46:01 +0200

    added express-validator

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const usersController = require("../controllers/UserController.js");
 const verifyToken = require("../middlewares/verifyToken.js");
+const loginValidation = require("../validations/loginValidation.js");
+const validate = require("../middlewares/validate.js");
 
 const router = express.Router();
 
-router.get("/", verifyToken, usersController.getUsers); 
-router.get("/:id", verifyToken, usersController.getUserById); 
-router.post(
-  "/",
-  usersController.SignUp
-); 
-router.post("/login", usersController.Login);
-router.patch("/:id",usersController.updateProfile)
-router.post("/forgotPassword",usersController.forgetPassword)
-router.post("/resetPassword",usersController.resetPassword)
+router.get("/", verifyToken, usersController.getUsers);
+router.get("/:id", verifyToken, usersController.getUserById);
+router.post("/", usersController.SignUp);
+router.post("/login", loginValidation, validate, usersController.Login);
+router.patch("/:id", usersController.updateProfile);
+router.post("/forgotPassword", usersController.forgetPassword);
+router.post("/resetPassword", usersController.resetPassword);
 
 module.exports = router;
