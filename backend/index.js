6dacb6eddfa38b26a23160457e387577dbeb64ff
OuachouX7 <ouachou9@gmail.com>
2025-05-07 01:34:09 +0200

    added validation

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,17 @@ const limiter = require("./middlewares/limiter.js");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static("public"));
-app.use(limiter)
+app.use(limiter);
 
 const server = http.createServer(app);
 
--- backend/routes/matchRoute.js
+++ backend/routes/matchRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const matchController = require("../controllers/MatchController.js");
 const verifyToken = require("../middlewares/verifyToken.js");
 const { createGameValidator } = require("../validations/matchValidation.js");
+const validate = require("../middlewares/validate.js");
 
 const router = express.Router();
 
