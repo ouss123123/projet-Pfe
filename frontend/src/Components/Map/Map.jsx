c65cc9d042e51bbd1db19bf70e594008ca9ab338
OuachouX7 <ouachou9@gmail.com>
2025-05-13 20:53:20 +0200

    map

--- frontend/src/Components/Map/Map.jsx
+++ frontend/src/Components/Map/Map.jsx
@@ -1,39 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const Map = ({ location }) => {
-  const [position, setPosition] = useState([31.7917, -7.0926]);
-  useEffect(() => {
-    try {
-      location &&
-        fetch(
-          `https://nominatim.openstreetmap.org/search?format=json&q=${location}`
-        )
-          .then((t) => t.json())
-          .then((t) => {
-            t.length > 0 &&
-              setPosition([parseFloat(t[0].lat), parseFloat(t[0].lon)]);
-          })
-          .catch((t) => console.error("Error fetching location:", t));
-    } catch (t) {
-      console.log(t);
-    }
-  }, [location]);
+const Map = ({ lat, lng }) => {
+  const position = [lat || 31.7917, lng || -7.0926];
+
   return (
     <div className="z-[-1] w-full">
-      {position && (
-        <MapContainer
-          center={position}
-          zoom={4}
-          style={{ height: "400px", width: "100%" }}
-        >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={position}>
-            <Popup>{location || "Selected Location"}</Popup>
-          </Marker>
-        </MapContainer>
-      )}
+      <MapContainer
+        center={position}
+        zoom={6}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={position}>
+          <Popup>{`Lat: ${lat}, Lng: ${lng}`}</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
