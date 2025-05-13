214dbe48bfce5a320db1c0152501197cea0033f5
OuachouX7 <ouachou9@gmail.com>
2025-05-13 20:34:18 +0200

    api fixed

--- backend/controllers/StadiumController.js
+++ backend/controllers/StadiumController.js
@@ -2,31 +2,31 @@ const asyncWrapper = require("../middlewares/asyncWrapper.js");
 const Stadium = require("../models/Stadium");
 
 const createStadium = asyncWrapper(async (req, res) => {
-  const { name, location, capacity } = req.body;
+  const { name } = req.body;
+  const { longitude, latitude } = req.body.location;
 
-  if (!name || !location || !capacity) {
+  if (!name) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  const stadium = await Stadium.create({ name, location, capacity });
+  const stadium = await Stadium.create({ name, longitude , latitude });
 
   return res.status(201).json(stadium);
-
 });
 
-const deleteStadium = asyncWrapper(async(req,res) => {
-    const id = req.params.id;
+const deleteStadium = asyncWrapper(async (req, res) => {
+  const id = req.params.id;
 
-    const stadium = await Stadium.deleteOne({ _id: id });
-    if (!stadium) {
-        return res.status(404).json({ message: "Stadium not found" });
-    }
-    return res.status(200).json({
-        message : "stadium Deleted Successfully"
-    })
-})
+  const stadium = await Stadium.deleteOne({ _id: id });
+  if (!stadium) {
+    return res.status(404).json({ message: "Stadium not found" });
+  }
+  return res.status(200).json({
+    message: "stadium Deleted Successfully",
+  });
+});
 
 module.exports = {
-    createStadium,
-    deleteStadium
-}
+  createStadium,
+  deleteStadium,
+};
