e0abada04deca9dd7c1695dfabe4fc4f715a690e
OuachouX7 <ouachou9@gmail.com>
2025-05-20 19:16:55 +0200

    added validator for messages

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const MessageController = require("../controllers/MessageController.js");
 const verifyToken = require("../middlewares/verifyToken.js");
+import { validate } from "../models/Message.js";
+import {
+  getMessagesValidator,
+  sendMessagesValidator,
+} from "../validations/messageValidator.js";
 
 const router = express.Router();
 
-router.post("/get", verifyToken, MessageController.getMessages);
-router.post("/send", verifyToken, MessageController.sendMessages);
+router.post(
+  "/get",
+  verifyToken,
+  getMessagesValidator,
+  validate,
+  MessageController.getMessages
+);
+router.post(
+  "/send",
+  verifyToken,
+  sendMessagesValidator,
+  validate,
+  MessageController.sendMessages
+);
 
 module.exports = router;
